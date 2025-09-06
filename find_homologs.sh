a6a2f5075264651ab12f75a0302f7b05ceb126b7
Grace Wang <140844505+gswang1@users.noreply.github.com>
2025-09-06 14:33:04 -0400

    Added draft blast script

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,3 +4,6 @@
 query_seq=$1
 subject_seq=$2
 outfile=$3
+
+tblastn -query $query_seq -subject $subject_seq -task tblastn -outfmt "6 pident length qlen" | awk "$1 > 30 && $2 > 0.9*$3" > $outfile
+wc -l < $outfile
