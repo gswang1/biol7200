de2a907dc5b5d0f721f6a1fd3a8e4a93581bc10d
Grace Wang <140844505+gswang1@users.noreply.github.com>
2025-09-08 22:11:45 -0400

    Added identifying information to output file

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,5 +5,5 @@ query_seq=$1
 subject_seq=$2
 outfile=$3
 
-tblastn -query $query_seq -subject $subject_seq -task tblastn -outfmt "6 pident length qlen" | awk '$1 > 30 && $2 > 0.9 * $3' > $outfile
+tblastn -query $query_seq -subject $subject_seq -task tblastn -outfmt "6 std qlen" | awk '$3 > 30 && $4 > 0.9 * $13' > $outfile
 wc -l < $outfile
